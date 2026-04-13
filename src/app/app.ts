1fbbf1ca0d7eec2c9021ca25491d54ffa213aa75
Thomas Többe <toebbe.thomas@outlook.de>
2026-04-13 13:13:20 +0200

    feat: project ground settings from DA importet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
+import { ProductList } from './shared/components/product-list/product-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, ProductList],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
