e38cdb9c2e4b2e0945a2755d22ed4296eaea39dc
Thomas Többe <toebbe.thomas@outlook.de>
2026-04-20 14:20:35 +0200

    feat: product List fir for loop

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,12 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
 import { ProductList } from './shared/components/product-list/product-list';
 import { ProductDetail } from './shared/components/product-detail/product-detail';
- 
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Header, ProductList, ProductDetail],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('product-list');
