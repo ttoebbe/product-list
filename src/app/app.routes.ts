c7d9e719949828f735ab93a9d8149e35830bad14
Thomas Többe <toebbe.thomas@outlook.de>
2026-05-06 10:12:31 +0200

    feat: form for new Products

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ProductDetail } from './shared/components/product-detail/product-detail';
 import { ProductList } from './shared/components/product-list/product-list';
+import { ProductForm } from './shared/components/product-form/product-form';
 
 export const routes: Routes = [
     {path: "", component: ProductList}, //rootebene
-    {path: "detail/:name", component: ProductDetail} //rootebene
+    {path: "detail/:name", component: ProductDetail}, //rootebene
+    {path:"productform", component:ProductForm} //rootebene
 ];
