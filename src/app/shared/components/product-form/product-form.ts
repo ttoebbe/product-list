a86f6a1b60cc9d2f296dc8b0d5a5fb1515bddda6
Thomas Többe <toebbe.thomas@outlook.de>
2026-05-06 10:32:08 +0200

    feat: form Group with submit

--- src/app/shared/components/product-form/product-form.ts
+++ src/app/shared/components/product-form/product-form.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './product-form.html',
   styleUrl: './product-form.scss',
 })
-export class ProductForm {}
+export class ProductForm {
+  productForm = new FormGroup({
+    name: new FormControl('n/a'),
+    price: new FormControl(0.00),
+    description: new FormControl('n/a'),
+    specs: new FormControl('n/a'),
+    stock: new FormControl(0),
+  });
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.log(this.productForm.value);
+  }
+
+}
